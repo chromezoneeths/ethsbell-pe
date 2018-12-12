86eff0a1fedbfcf2ebca3cae83c2a5f3e471374c
Albert Portnoy <albert.portnoy@gmail.com>
2018-12-12 14:54:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,31 +6,31 @@ function sel(query) {
 
 function getParameterByName(name, url) {
   if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, '\\$&');
-  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
     results = regex.exec(url);
   if (!results) return null;
-  if (!results[2]) return '';
-  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-var mocktime = getParameterByName('mock_time');
+var mocktime = getParameterByName("mock_time");
 
 if (mocktime == null) {
-  mocktime = '';
+  mocktime = "";
 } else {
-  mocktime = '?mock_time=' + mocktime;
+  mocktime = "?mock_time=" + mocktime;
 }
 
 var dataURL = "https://api.ethsbell.xyz/data" + mocktime;
 var displayURL = "https://api.ethsbell.xyz/display" + mocktime;
 
-//FIND NEW SOLUTION THAT USES HTTPS!!!
-var sheetURL = "https://spreadsheets.google.com/feeds/list/1T-HUAINDX69-UYUHhOO1jVjZ_Aq0Zqi1z08my0KHzqU/1/public/values?alt=json";
+var sheetURL =
+  "https://spreadsheets.google.com/feeds/list/1T-HUAINDX69-UYUHhOO1jVjZ_Aq0Zqi1z08my0KHzqU/1/public/values?alt=json";
 // Function to get data from ETHSBell
 
 function run(msg) {
-  var datadiv = document.getElementById('data');
+  var datadiv = document.getElementById("data");
   var data = JSON.parse(msg);
   var responseObj = {};
   var rows = [];
@@ -41,7 +41,7 @@ function run(msg) {
     var newRow = {};
     var queried = false;
     for (var j = 0; j < keys.length; j++) {
-      var gsxCheck = keys[j].indexOf('gsx$');
+      var gsxCheck = keys[j].indexOf("gsx$");
       if (gsxCheck > -1) {
         var key = keys[j];
         var name = key.substring(4);
@@ -67,10 +67,10 @@ function run(msg) {
     }
   }
   if (true) {
-    responseObj['columns'] = columns;
+    responseObj["columns"] = columns;
   }
   if (true) {
-    responseObj['rows'] = rows;
+    responseObj["rows"] = rows;
   }
   table(responseObj);
 }
@@ -87,21 +87,19 @@ function ajax(theUrl, callback) {
 
 function ethsbellDiv(text) {
   //Sets the innerHTML of the bell div to the bell when updated
-  var bellDiv = sel('#ethsbell');
+  var bellDiv = sel("#ethsbell");
   bellDiv.innerHTML = text;
 }
 
 // ETHS BELL function
 
 function ethsbell() {
-
   //Update table
   ajax(sheetURL, run);
   //update bell
-  console.log('Bell schedule update');
+  console.log("Bell schedule update");
   ajax(displayURL, ethsbellDiv);
   ajax(dataURL, updateBoard);
-
 }
 
 // strip html tags from string (for later)
@@ -127,13 +125,23 @@ ethsbell();
 //Location board in form of JS Object
 
 var Locations = {
-  "div": function() {
-    return sel('#board');
+  div: function() {
+    return sel("#board");
   }
 };
 
-var periodList = ["Early Bird", "1st Period", "2nd Period", "3rd Period", "4th Period", "5th Period", "6th Period", "7th Period", "8th Period", "9th Period"];
-
+var periodList = [
+  "Early Bird",
+  "1st Period",
+  "2nd Period",
+  "3rd Period",
+  "4th Period",
+  "5th Period",
+  "6th Period",
+  "7th Period",
+  "8th Period",
+  "9th Period"
+];
 
 // Add perids to array to init
 
@@ -141,23 +149,22 @@ for (var k = 0; k < periodList.length; k++) {
   Locations[periodList[k]] = {};
 }
 
-
 //Function to update the table
 
 function convertTo12Hour(time) {
   if (time.length == 4) {
-    time = '0' + time;
+    time = "0" + time;
   }
   var hours = parseInt(time.substr(0, 2));
   var minutes = parseInt(time.substr(3, 4));
-  minutes += '';
+  minutes += "";
   if (hours > 12) {
     hours -= 12;
   }
   if (minutes.length == 1) {
-    minutes = '0' + minutes;
+    minutes = "0" + minutes;
   }
-  return hours + ':' + minutes;
+  return hours + ":" + minutes;
 }
 //global variables for board
 var currentPeriod;
@@ -165,12 +172,11 @@ var response;
 
 function updateBoard(data) {
   response = JSON.parse(data);
-  if (response.dayOfWeek == 'Saturday' || response.dayOfWeek == 'Sunday') {
-    Locations.div().innerHTML = 'Have a nice day!';
+  if (response.dayOfWeek == "Saturday" || response.dayOfWeek == "Sunday") {
+    Locations.div().innerHTML = "Have a nice day!";
   } else {
-
     var div = Locations.div(); // Where table will be put
-    div.innerHTML = ''; // Clear div
+    div.innerHTML = ""; // Clear div
     // Add empty table to page
     var table = document.createElement("TABLE");
     div.appendChild(table);
@@ -179,8 +185,7 @@ function updateBoard(data) {
 
     var periodTimes = response.schedule.period_array;
 
-
-    table = sel('#board table'); // Define the table for future use
+    table = sel("#board table"); // Define the table for future use
     for (var a = 1; a <= periodCount; a++) {
       var row = document.createElement("tr"); // Add a new row to the table
       row.setAttribute("id", "period" + (a - 1));
@@ -205,29 +210,34 @@ function updateBoard(data) {
           }
         });
 
-
-
         var timeCell = document.createElement("td"); // Cell with period times
-        timeCell.innerHTML = convertTo12Hour(startTime) + ' - ' + convertTo12Hour(endTime);
+        timeCell.innerHTML =
+          convertTo12Hour(startTime) + " - " + convertTo12Hour(endTime);
         row.appendChild(timeCell);
       }
       // Add each teacher-location cell to the row
       for (var b = 0; b < Object.keys(period).length; b++) {
         var cell = document.createElement("td"); // New cell
-        cell.innerHTML = Object.entries(period)[b][0] /*Teacher name*/ + ': ' + Object.entries(period)[b][1] /*Location name*/ ;
+        cell.classList += "maincell";
+        cell.innerHTML =
+          Object.entries(period)[b][0] /*Teacher name*/ +
+          ": " +
+          Object.entries(period)[b][1] /*Location name*/;
         row.appendChild(cell); //Add cell to row
-
       }
 
       table.appendChild(row); //Add row to table
     }
+      search();
+
 
     // Highlight current period
     if (response !== undefined) {
       response = response.theSlot; // Get period from json object
       var index = periodList.indexOf(response); //Find period #
-      if (index !== -1) { // If period exists
-        sel('#period' + (index + '')).className += "current";
+      if (index !== -1) {
+        // If period exists
+        sel("#period" + (index + "")).className += "current";
       }
     }
   }
@@ -243,15 +253,36 @@ function table(json) {
   var data = json.rows;
   for (var j = 0; j < teacherArray.length; j++) {
     for (var k = 0; k < periodList.length; k++) {
-      if (Object.values(data[j])[k + 1] !== 'null') {
-        Locations[periodList[k]][data[j].teachername] = Object.values(data[j])[k + 1];
+      if (Object.values(data[j])[k + 1] !== "null") {
+        Locations[periodList[k]][data[j].teachername] = Object.values(data[j])[
+          k + 1
+        ];
       }
     }
+    search();
   }
-
-
 }
 
 // Create table (INIT)
 
 //updateBoard();
+function search() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("board");
+  td = table.querySelectorAll("td.maincell");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < td.length; i++) {
+    current = td[i];
+    txtValue = current.textContent || current.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      current.style.display = "";
+    } else {
+      current.style.display = "none";
+    }
+  }
+}
+
+ethsbell();
